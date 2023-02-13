1e13ac3b101425c579bc6a4097f985eac07f3ea0
Chisthia Khan <chisti.khan1999@gmail.com>
2023-02-13 22:00:04 +0600

    first commit

---
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.broadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Switch;
+
+import com.example.broadcastreceiver.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        dropdownManager();
+    }
+
+    private void dropdownManager() {
+        String items []= new String[]{
+                "Custom broadcast receiver",
+                "Wifi RTT state change receiver",
+                "System battery notification receiver"
+        };
+
+
+        ArrayAdapter<String> adapter= new ArrayAdapter<>(
+                MainActivity.this,
+                R.layout.drop_down_text,
+                items
+        );
+
+        binding.dropDownText.setAdapter(adapter);
+
+        binding.btnProceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                switch (binding.dropDownText.getText().toString()){
+                    case "Custom broadcast receiver":
+                        //startActivity(new Intent(MainActivity.this,BatteryBroadcastReceiver.class));
+                        break;
+                    case "Wifi RTT state change receiver":
+                        startActivity(new Intent(MainActivity.this,WifiRttStateChangeReceiver.class));
+                        break;
+                    case "System battery notification receiver":
+                        startActivity(new Intent(MainActivity.this,UserInputBatteryPercentage.class));
+                        break;
+                    default:
+                        break;
+
+                }
+
+            }
+        });
+    }
+
+
+
+
+}
